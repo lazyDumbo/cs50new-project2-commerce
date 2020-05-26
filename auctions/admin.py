ac574d14a0e4f9d45fadcefbb15d1fd94a1f5b94
iqlipsechar@gmail.com <iqlipsechar@gmail.com>
2020-05-26 05:47:24 +0530

    dajbsdk

---
+++ auctions/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import User,Listing,Comment,Bids,Watchlist
+
+admin.site.register(User)
+admin.site.register(Listing)
+admin.site.register(Comment)
+admin.site.register(Bids)
+admin.site.register(Watchlist)
+#django.admin.site.register(User)
